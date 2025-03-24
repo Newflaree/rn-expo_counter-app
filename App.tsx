750a326b535ad5f980595fea82015e4d1f13a8eb
Camilo López <camiloj.lopez79@gmail.com>
2025-03-23 23:24:59 -0300

    Estilos y botones

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
 
 export default function App() {
+  const [ count, setCount ] = useState(10);
+
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo</Text>
+      <Text style={styles.textHuge}>{ count }</Text>
+
+      <Pressable
+        style={ styles.floatingButton }
+        onPress={ () => setCount( count + 1 ) }
+        onLongPress={ () => setCount(0) }
+      >
+        <Text style={{ color: 'white', fontSize: 20 }}>+ 10</Text>
+      </Pressable>
+
+      <TouchableOpacity>
+        <Text>+ 10</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +39,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textHuge: {
+    fontSize: 120,
+    fontWeight: '100'
+  },
+  floatingButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: '#65558F',
+    padding: 20,
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    shadowRadius: 3,
+    shadowOpacity: 0.3,
+    elevation: 3
+  }
 });
